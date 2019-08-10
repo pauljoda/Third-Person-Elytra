9ae2c5cb0dc74469d239070216499f8c228b78f9
Paul Davis <pauldavis101@gmail.com>
2019-08-10 15:52:52 -0500

    1.13 Update
    Along with the port to newer minecraft, added config to toggle in-game. TODO: re-enable FML config gui

--- src/main/java/com/pauljoda/thirdpersonelytra/ThirdPersonElytra.java
+++ src/main/java/com/pauljoda/thirdpersonelytra/ThirdPersonElytra.java
@@ -1,16 +1,36 @@
 package com.pauljoda.thirdpersonelytra;
 
 import com.pauljoda.thirdpersonelytra.event.ClientRenderEvent;
+import com.pauljoda.thirdpersonelytra.event.KeyPressedEvent;
 import com.pauljoda.thirdpersonelytra.lib.Reference;
+import com.pauljoda.thirdpersonelytra.manager.ConfigManager;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(value = Reference.MOD_ID)
 public class ThirdPersonElytra {
+    // Variable for is enabled
+    public static boolean isToggleEnabled;
 
-    @Mod.EventHandler
-    public static void preInit(FMLPreInitializationEvent event) {
+    public ThirdPersonElytra() {
+        // Registers the event to swap camera
         MinecraftForge.EVENT_BUS.register(ClientRenderEvent.INSTANCE);
+
+        // Registers the toggle keys
+        MinecraftForge.EVENT_BUS.register(KeyPressedEvent.INSTANCE);
+
+        // Inline method, just want to update local values on changes
+        FMLJavaModLoadingContext.get().getModEventBus().addListener((ModConfig.ModConfigEvent event) -> {
+            final ModConfig config = event.getConfig();
+            // Rebake the configs when they change
+            if (config.getSpec() == ConfigManager.CLIENT_SPEC) {
+                ConfigManager.updateValues(config);
+            }
+        });
+        KeyPressedEvent.INSTANCE.registerBindings();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigManager.CLIENT_SPEC);
     }
 }
--- src/main/java/com/pauljoda/thirdpersonelytra/event/ClientRenderEvent.java
+++ src/main/java/com/pauljoda/thirdpersonelytra/event/ClientRenderEvent.java
@@ -1,25 +1,26 @@
 package com.pauljoda.thirdpersonelytra.event;
 
 
+import com.pauljoda.thirdpersonelytra.ThirdPersonElytra;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class ClientRenderEvent {
     public static ClientRenderEvent INSTANCE = new ClientRenderEvent();
-    private int lastMode = (Minecraft.getMinecraft()).gameSettings.thirdPersonView;
+    private int lastMode = (Minecraft.getInstance()).gameSettings.thirdPersonView;
     private boolean flyingLastTick = false;
 
     @SubscribeEvent
     public void clientRender(TickEvent.RenderTickEvent event) {
-        if ((Minecraft.getMinecraft()).world != null) {
-            EntityPlayerSP player = (Minecraft.getMinecraft()).player;
+        if (ThirdPersonElytra.isToggleEnabled && (Minecraft.getInstance()).world != null) {
+            EntityPlayerSP player = (Minecraft.getInstance()).player;
             if (player.isElytraFlying() && !this.flyingLastTick) {
-                this.lastMode = (Minecraft.getMinecraft()).gameSettings.thirdPersonView;
-                (Minecraft.getMinecraft()).gameSettings.thirdPersonView = 1;
+                this.lastMode = (Minecraft.getInstance()).gameSettings.thirdPersonView;
+                (Minecraft.getInstance()).gameSettings.thirdPersonView = 1;
             } else if (!player.isElytraFlying() && this.flyingLastTick) {
-                (Minecraft.getMinecraft()).gameSettings.thirdPersonView = this.lastMode;
+                (Minecraft.getInstance()).gameSettings.thirdPersonView = this.lastMode;
             }
             this.flyingLastTick = player.isElytraFlying();
         }
