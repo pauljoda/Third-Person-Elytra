9e98ab81f18f428a348500ade4a93de63145f52d
Paul Davis <pauldavis101@gmail.com>
2019-08-13 20:43:24 -0500

    Added sided checks

--- src/main/java/com/pauljoda/thirdpersonelytra/ThirdPersonElytra.java
+++ src/main/java/com/pauljoda/thirdpersonelytra/ThirdPersonElytra.java
@@ -4,11 +4,14 @@ import com.pauljoda.thirdpersonelytra.event.ClientRenderEvent;
 import com.pauljoda.thirdpersonelytra.event.KeyPressedEvent;
 import com.pauljoda.thirdpersonelytra.lib.Reference;
 import com.pauljoda.thirdpersonelytra.manager.ConfigManager;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import org.apache.logging.log4j.LogManager;
 
 @Mod(value = Reference.MOD_ID)
 public class ThirdPersonElytra {
@@ -16,21 +19,27 @@ public class ThirdPersonElytra {
     public static boolean isToggleEnabled;
 
     public ThirdPersonElytra() {
-        // Registers the event to swap camera
-        MinecraftForge.EVENT_BUS.register(ClientRenderEvent.INSTANCE);
+        // Only need to register on client, server should never have installed but just to be safe
+        if(FMLEnvironment.dist == Dist.CLIENT) {
+            // Registers the event to swap camera
+            MinecraftForge.EVENT_BUS.register(ClientRenderEvent.INSTANCE);
 
-        // Registers the toggle keys
-        MinecraftForge.EVENT_BUS.register(KeyPressedEvent.INSTANCE);
+            // Registers the toggle keys
+            MinecraftForge.EVENT_BUS.register(KeyPressedEvent.INSTANCE);
 
-        // Inline method, just want to update local values on changes
-        FMLJavaModLoadingContext.get().getModEventBus().addListener((ModConfig.ModConfigEvent event) -> {
-            final ModConfig config = event.getConfig();
-            // Rebake the configs when they change
-            if (config.getSpec() == ConfigManager.CLIENT_SPEC) {
-                ConfigManager.updateValues(config);
-            }
-        });
-        KeyPressedEvent.INSTANCE.registerBindings();
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigManager.CLIENT_SPEC);
+            // Inline method, just want to update local values on changes
+            FMLJavaModLoadingContext.get().getModEventBus().addListener((ModConfig.ModConfigEvent event) -> {
+                final ModConfig config = event.getConfig();
+                // Rebake the configs when they change
+                if (config.getSpec() == ConfigManager.CLIENT_SPEC) {
+                    ConfigManager.updateValues(config);
+                }
+            });
+            KeyPressedEvent.INSTANCE.registerBindings();
+            ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, ConfigManager.CLIENT_SPEC);
+        } else { // Warn servers about running client side mod
+            LogManager.getLogger(Reference.MOD_ID).error("Warning: You are running Third Person Elytra on a server." +
+                    " This should be an issue, but is a client side only mod");
+        }
     }
 }
