bfbe3785ca943b5f21f661afbfac1fa287d62a55
Paul Davis <pauldavis101@gmail.com>
2020-08-22 23:37:19 -0500

    Build for 1.16

--- src/main/java/com/pauljoda/thirdpersonelytra/event/ClientRenderEvent.java
+++ src/main/java/com/pauljoda/thirdpersonelytra/event/ClientRenderEvent.java
@@ -4,12 +4,13 @@ package com.pauljoda.thirdpersonelytra.event;
 import com.pauljoda.thirdpersonelytra.ThirdPersonElytra;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.settings.PointOfView;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class ClientRenderEvent {
     public static ClientRenderEvent INSTANCE = new ClientRenderEvent();
-    private int lastMode = (Minecraft.getInstance()).gameSettings.thirdPersonView;
+    private PointOfView lastMode = (Minecraft.getInstance()).gameSettings.func_243230_g();
     private boolean flyingLastTick = false;
 
     @SubscribeEvent
@@ -17,10 +18,10 @@ public class ClientRenderEvent {
         if (ThirdPersonElytra.isToggleEnabled && (Minecraft.getInstance()).world != null) {
             ClientPlayerEntity player = (Minecraft.getInstance()).player;
             if (player.isElytraFlying() && !this.flyingLastTick) {
-                this.lastMode = (Minecraft.getInstance()).gameSettings.thirdPersonView;
-                (Minecraft.getInstance()).gameSettings.thirdPersonView = 1;
+                this.lastMode = (Minecraft.getInstance()).gameSettings.func_243230_g();
+                (Minecraft.getInstance()).gameSettings.func_243229_a(PointOfView.THIRD_PERSON_BACK);
             } else if (!player.isElytraFlying() && this.flyingLastTick) {
-                (Minecraft.getInstance()).gameSettings.thirdPersonView = this.lastMode;
+                (Minecraft.getInstance()).gameSettings.func_243229_a(this.lastMode);
             }
             this.flyingLastTick = player.isElytraFlying();
         }
